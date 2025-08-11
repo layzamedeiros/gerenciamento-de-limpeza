9f03c07facf37edd90d65ef65d5abc94462e3263
layzamedeiros <165849168+layzamedeiros@users.noreply.github.com>
2025-08-11 14:23:23 -0300

    feat: configurando tema global com styled-components e adicionado componente Loading

--- App.tsx
+++ App.tsx
@@ -1,7 +1,17 @@
-import { Home } from "@screens/Home";
+import { ThemeProvider } from 'styled-components/native';
+import { useFonts, Inter_400Regular, Inter_500Medium, Inter_700Bold } from '@expo-google-fonts/inter';
+
+import { Home } from '@screens/Home';
+import { Loading } from '@components/Loading';
+
+import theme from '@theme/index';
 
 export default function App() {
+  const [ fontsLoaded ] = useFonts({ Inter_400Regular, Inter_500Medium, Inter_700Bold });
+
   return (
-    <Home />
+    <ThemeProvider theme={theme}>
+      { fontsLoaded ? <Home /> : <Loading /> }
+    </ThemeProvider>
   );
 }
---
+++ src/theme/index.ts
@@ -0,0 +1,25 @@
+export default {
+  COLORS: {
+    PRIMARY: "#004A8D",    
+    ACCENT: "#F7941D",     
+    
+    BACKGROUND: "#FBFBFB", 
+    SURFACE: "#FFFFFF",   
+    PRIMARY_LIGHT: "#D6E4F2",
+    DANGER: "#DC2626",     
+
+    TITLE: "#1A1A1A",        
+    SUBTITLE: "#6B7280",     
+    BORDER: "#D4D4D8",       
+    PLACEHOLDER: "#A1A1AA",  
+    DISABLED: "#F4F4F5",     
+    
+    WHITE: "#FFFFFF"         
+  },
+
+  FONT_FAMILY: {
+    REGULAR: "Inter_400Regular",
+    MEDIUM: "Inter_500Medium",
+    BOLD: "Inter_700Bold"
+  }
+};
